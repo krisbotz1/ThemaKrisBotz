0babecce3bbd9efca27ffe79bffd52f385440d8b
bahan <128270036+lexxystore@users.noreply.github.com>
2023-03-19 10:05:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/lexxystore/ThemaKrisBotz/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/bahan/ThemaKrisBotz/main/repair.sh)
 }
 
 restoreBackUp(){
